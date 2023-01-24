0bd333c95b0a79fb65b4d94fac993b064df103af
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-24 12:25:37 +0100

    test10 done

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -9,12 +9,15 @@ private:
     int _wealth_amount;
     int _stock_size;
     int _debt_amount;
+    int _experience;
 
 public:
     PlushStore(const std::string &name) : _name{name},
                                           _wealth_amount{0},
                                           _stock_size{0},
-                                          _debt_amount{0}
+                                          _debt_amount{0},
+                                          _experience{0}
+
     {
     }
 
@@ -44,12 +47,20 @@ public:
         return _debt_amount;
     }
 
-    void make_plush(int cost)
+    int make_plush(int cost)
     {
         if (_wealth_amount > 0)
         {
             _stock_size++;
+            _experience++;
             _wealth_amount = cost > _wealth_amount ? 0 : _wealth_amount - cost;
         }
+        int const value = cost + std::max(_experience, cost * _experience / 100);
+        return value;
+    }
+
+    int get_experience() const
+    {
+        return _experience;
     }
 };
