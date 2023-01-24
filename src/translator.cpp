cc76e6a25ce112a68aeb94eeb013a2a7ae99a650
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-24 04:55:30 +0100

    ex2 1) done

--- src/translator.cpp
+++ src/translator.cpp
@@ -10,15 +10,40 @@ std::set<std::string> make_exit_commands()
         "exit"};
 }
 
-int main()
+bool execute_command(std::istream &input)
 {
     using namespace std;
     const auto exit_commands = make_exit_commands();
     string command = {};
-    while (exit_commands.count(command) == 0)
+    input >> command;
+
+    if (exit_commands.count(command) != 0)
     {
-        cout << "Enter command : " << std::endl;
-        cin >> command;
+        return false;
+    }
+
+    if (command == "add")
+    {
+        string w1;
+        input >> w1;
+        string w2;
+        input >> w2;
+        cout << w1 + " => " + w2 << endl;
+    }
+
+    return true;
+}
+
+int main()
+{
+    using namespace std;
+    while (true)
+    {
+        cout << "Enter command : " << endl;
+        if (!execute_command(cin))
+        {
+            break;
+        }
     }
 
     return 0;
