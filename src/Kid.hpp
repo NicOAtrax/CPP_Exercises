c36dc2f116945f2156d61ac7bcff408f070ca1f1
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-01-24 12:31:33 +0100

    test11 done

--- src/Kid.hpp
+++ src/Kid.hpp
@@ -1,1 +1,25 @@
 #pragma once
+#include <iostream>
+
+class Kid
+{
+private:
+    std::string _name;
+    int _money;
+
+public:
+    Kid(std::string name, int money) : _name{name},
+                                       _money{money}
+    {
+    }
+
+    std::string get_name() const
+    {
+        return _name;
+    }
+
+    int get_money() const
+    {
+        return _money;
+    }
+};
