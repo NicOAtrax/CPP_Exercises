bae9ca20bc47f72a2e93593de1703b67e8b5013e
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-02-09 22:23:41 +0100

    test 4 et 5 done

--- src/PC.cpp
+++ src/PC.cpp
@@ -1,1 +1,13 @@
 #include "PC.h"
+
+std::vector<PokemonPtr> &PC::pokemons()
+{
+    return _pokemons;
+}
+void PC::transfer(PokemonPtr pokemon)
+{
+    if (pokemon != nullptr)
+    {
+        _pokemons.push_back(std::move(pokemon));
+    }
+}
