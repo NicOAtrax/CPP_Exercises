34056b468cc6cf816f4ae18271feb22d3d465058
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-02-14 09:23:26 +0100

    test 2 done

--- src/Card.hpp
+++ src/Card.hpp
@@ -8,7 +8,7 @@ enum class CardType
     Trap
 };
 
-inline std::string to_string(CardType type)
+inline std::string to_string(const CardType type)
 {
     switch (type)
     {
@@ -22,3 +22,50 @@ inline std::string to_string(CardType type)
 
     return "";
 }
+
+class Card
+{
+private:
+    const std::string _id;
+    CardType _type;
+    std::string _name;
+    std::string _description;
+
+public:
+    Card(std::string id, CardType type) : _id{id},
+                                          _type{type},
+                                          _name{""},
+                                          _description{""}
+    {
+    }
+
+    const std::string &get_id() const
+    {
+        return _id;
+    }
+
+    CardType get_type() const
+    {
+        return _type;
+    }
+
+    const std::string &get_name() const
+    {
+        return _name;
+    }
+
+    const std::string &get_description() const
+    {
+        return _description;
+    }
+
+    void set_name(const std::string name)
+    {
+        _name = name;
+    }
+
+    void set_description(const std::string description)
+    {
+        _description = description;
+    }
+};
