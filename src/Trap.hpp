7778236c1b302b4284dfc9dac6d08f2afe420631
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-03-01 13:42:29 +0100

    Saves changes

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,42 @@
 #pragma once
+#include <string>
+#include "Card.hpp"
+
+enum class TrapType
+{
+    Normal,
+    Continuous,
+    Counter
+};
+
+inline std::string to_string(const TrapType type)
+{
+    switch (type)
+    {
+    case TrapType::Normal:
+        return "Normal";
+    case TrapType::Continuous:
+        return "Continuous";
+    case TrapType::Counter:
+        return "Counter";
+    }
+    return "";
+};
+
+class Trap : public Card
+{
+private:
+    TrapType _type;
+
+public:
+    Trap(std::string id, std::string name, TrapType type) : Card{std::move(id), CardType::Trap},
+                                                            _type{type}
+    {
+        set_name(std::move(name));
+    }
+
+    TrapType get_trap_type() const
+    {
+        return _type;
+    }
+};
