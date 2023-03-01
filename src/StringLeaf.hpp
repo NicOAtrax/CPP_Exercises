398caf60296b146a1490bfb2ae0248e257b47e43
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-03-01 14:46:47 +0100

    test3 done

--- src/StringLeaf.hpp
+++ src/StringLeaf.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "Leaf.hpp"
+
+#include <string>
+
+class StringLeaf : public Leaf
+{
+private:
+    std::string _data;
+
+public:
+    StringLeaf(std::string data)
+        : Leaf { NodeKind::STRING }
+        , _data { data }
+    {}
+
+    std::string        print() const override { return "\"" + _data + "\""; }
+    const std::string& data() const { return _data; }
+};
