77ace12585fda87b3a0789b2d617544284376224
Atrax Nicolas <nicolas.atrax9@gmail.com>
2023-03-14 12:16:42 +0100

    test1 done

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,19 @@
 #include "Kitchen.hpp"
+
+#include <utility>
+
+const Unit& Kitchen::register_unit(Unit unit)
+{
+    auto [it, emplaced] = _units.emplace(std::move(unit));
+    return *it;
+}
+
+const Unit* Kitchen::find_unit(const std::string& name)
+{
+    auto it = _units.find(Unit { name });
+    if (it != _units.end())
+    {
+        return &(*it);
+    }
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,19 @@
 #pragma once
+#include "../lib/Unit.hpp"
+
+#include <set>
+
+struct comparator
+{
+    bool operator()(Unit unit1, Unit unit2) const { return unit1.name < unit2.name; }
+};
 
 class Kitchen
-{};
+{
+private:
+    std::set<Unit, comparator> _units;
+
+public:
+    const Unit& register_unit(Unit unit);
+    const Unit* find_unit(const std::string& name);
+};
